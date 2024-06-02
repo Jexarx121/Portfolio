75991e1bde5f49b4fe0f63db7f5fe12840dfa3e6
Zhi <zjchen402@gmail.com>
2024-06-02 21:05:46 +0100

    feat: Made website responsive

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -3,10 +3,10 @@ import Title from "../helpers/title/title";
 
 const Contact = () => {
   return (
-    <div className="w-[80%] m-auto mt-24 mb-48" id="contact">
+    <div className="lg:w-[80%] sm:w-[90%] w-[100%] m-auto mt-24 mb-48 sm:p-0 px-8" id="contact">
       <Title title="Connect"/>
       <Separator/>
-      <p className="w-[50%] mb-5 text-[20px]">
+      <p className="xl:w-[50%] lg:w-[60%] md:w-[75%] sm:w-[90%] w-[100%] mb-5 text-[20px]">
         If you want to get in touch, my inbox is always open. Feel free to ask any questions and I'll try my best to get back to you!
       </p>
       <button className="text-[20px] text-white bg-[#2E9CCA] px-5 py-2 rounded-md">Message</button>
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-row justify-between my-10">
-      <div className="flex flex-row ml-10">
-        <Link href="https://github.com/Jexarx121"><FontAwesomeIcon className="px-3 text-white text-xl" icon={faGithub}/></Link>
-        <Link href="https://www.linkedin.com/in/zjchen402/"><FontAwesomeIcon className="px-3 text-white text-xl" icon={faLinkedin}/></Link>
-        <FontAwesomeIcon className="px-3 text-white cursor-pointer text-xl" icon={faFile}/>
+    <footer className="flex sm:flex-row flex-col justify-between my-10">
+      <div className="flex flex-row sm:ml-10 ml-8">
+        <Link href="https://github.com/Jexarx121"><FontAwesomeIcon className="pr-3 text-white sm:text-xl text-2xl" icon={faGithub}/></Link>
+        <Link href="https://www.linkedin.com/in/zjchen402/"><FontAwesomeIcon className="px-3 text-white sm:text-xl text-2xl" icon={faLinkedin}/></Link>
+        <FontAwesomeIcon className="px-3 text-white cursor-pointer sm:text-xl text-2xl" icon={faFile}/>
       </div>
-      <div className="mr-10">
+      <div className="sm:mr-10 sm:m-0 m-8">
         <p className="text-xs">All rights reserved. Made loosely in Figma. Developed in Next.js, Typescript and Tailwind CSS.</p>
       </div>
     </footer>
