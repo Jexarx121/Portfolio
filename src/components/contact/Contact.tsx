ab7f0b3ecba6731e49bf27f0f8e7ea7503062cdb
Zhi <zjchen402@gmail.com>
2024-06-05 21:53:01 +0100

    feat: Added email link to contact button

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,5 +1,6 @@
 import Separator from "../helpers/separator/Separator";
 import Title from "../helpers/title/title";
+import Link from "next/link";
 
 const Contact = () => {
   return (
@@ -9,7 +10,7 @@ const Contact = () => {
       <p className="xl:w-[50%] lg:w-[60%] md:w-[75%] sm:w-[90%] w-[100%] mb-5 text-[20px]">
         If you want to get in touch, my inbox is always open. Feel free to ask any questions and I&#39;ll try my best to get back to you!
       </p>
-      <button className="text-[20px] text-white border-[#2E9CCA] border-2 px-5 py-2 rounded-md hover:bg-[#2E9CCA] hover:font-bold transition duration-300">Message</button>
+      <Link className="text-[20px] text-white border-[#2E9CCA] border-2 px-5 py-2 rounded-md hover:bg-[#2E9CCA] hover:font-bold transition duration-300 cursor-pointer" href="mailto:zjchen402@email.com">Message</Link>
     </div>
   )
 }
