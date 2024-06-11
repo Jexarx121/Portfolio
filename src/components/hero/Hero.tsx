eda33a0f8c2d35b9a16b62ee59fe7e2b3b202ce5
Zhi <zjchen402@gmail.com>
2024-06-11 13:42:28 +0100

    fix: Hero cursor animation fixed for different sizes

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % texts.length);
-    }, 4000);
+    }, 4500);
 
     return () => clearInterval(interval);
   }, [texts.length])
@@ -16,8 +16,8 @@ const Hero = () => {
   return (
     <div className="lg:w-[60%] md:w-[80%] sm:w-[90%] w-[100%] m-auto min-h-screen flex flex-col mt-48 sm:p-0 px-8">
       <h1 className="lg:my-6 md:my-5 my-4">Y&#39;hello, my name is</h1>
-      <h1 className="text-white lg:text-8xl md:text-7xl sm:text-6xl text-5xl font-bold lg:my-6 md:my-5 my-4">Zhi Jie Chen.</h1>
-      <h2 className="text-[#aaabb8] lg:text-8xl md:text-7xl sm:text-6xl text-5xl font-bold lg:my-6 md:my-5 my-4 py-2 typed"
+      <h1 className="text-white xl:text-8xl lg:text-[80px] md:text-7xl sm:text-6xl text-5xl font-bold lg:my-6 md:my-5 my-4">Zhi Jie Chen.</h1>
+      <h2 className="text-[#aaabb8] 2xl:text-8xl xl:text-7xl md:text-6xl text-5xl font-bold lg:my-6 md:my-5 my-4 py-2 typed"
         key={currentIndex}>
         {texts[currentIndex]}
       </h2>
